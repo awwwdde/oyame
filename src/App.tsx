436fd1bd380a7f92fbe9d4dcc795e8126390ab0c
awwwdde <sjuu1337@gmail.com>
2025-06-03 02:05:27 +0300

    0,4 // monochrome

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
+import MainPage from "./pages/main/MainPage"
 
 
 function App() {
   return (
     <>
-      OYAME [MONOCHROME] <br/>
-      AWWWDDE [SALONEE] <br/> 
-      KYOTO COLLECTION [2025 Q2] <br/>
-      自由は心をクリアにする。
+      <MainPage />
     </>
   )
 }
---
+++ src/pages/main/MainPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MainPage: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-end justify-start p-16 font-mono">
+      <div className="space-y-4">
+        <p className="text-2xl tracking-wider">
+          OYAME [MONOCHROME] <br/>
+          AWWWDDE [SALONEE] <br/>
+          KYOTO COLLECTION [2025 Q2]
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
